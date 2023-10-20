44af6580fe82a4835aa9093548f31ae02d101976
wao218 <wesleyosborne1@gmail.com>
2023-10-20 16:48:50 -0700

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by wesley osborne on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
