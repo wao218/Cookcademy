dbbe6cc3bef4d98f8ed51c88940d1362a9be28db
wao218 <wesleyosborne1@gmail.com>
2023-10-20 17:17:47 -0700

    Add Recipe Model

---
+++ Cookcademy/Recipe.swift
@@ -0,0 +1,48 @@
+//
+//  Recipe.swift
+//  Cookcademy
+//
+//  Created by wesley osborne on 10/20/23.
+//
+
+import Foundation
+
+struct Recipe {
+    var mainInformation: MainInformation
+    var ingredients: [Ingredient]
+    var directions: [Direction]
+}
+
+struct MainInformation {
+    var name: String
+    var description: String
+    var author: String
+    var category: Category
+    
+    enum Category: String, CaseIterable {
+        case breakfast = "Breakfast"
+        case lunch = "Lunch"
+        case dinner = "Dinner"
+        case dessert = "Dessert"
+    }
+}
+
+struct Ingredient {
+    var name: String
+    var quantity: Double
+    var unit: Unit
+    
+    enum Unit: String, CaseIterable {
+        case oz = "Ounces"
+        case g = "Grams"
+        case cups = "Cups"
+        case tbs = "Tablespoons"
+        case tsp = "Teaspoons"
+        case none = "No units"
+    }
+}
+
+struct Direction {
+    var description: String
+    var isOptional: Bool
+}
