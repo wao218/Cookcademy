029a4ad6883fd4621d0535c89487d940d04e74b0
wao218 <wesleyosborne1@gmail.com>
2023-10-21 19:23:32 -0700

    Refactor RecipeCategoryGridView and add CategoryView

--- Cookcademy/Views/RecipeCategoryGridView.swift
+++ Cookcademy/Views/RecipeCategoryGridView.swift
@@ -11,23 +11,31 @@ struct RecipeCategoryGridView: View {
     private var recipeData = RecipeData()
     
     var body: some View {
+        let columns = [GridItem(), GridItem()]
         NavigationView {
             ScrollView{
-                let columns = [GridItem(), GridItem()]
                 LazyVGrid(columns: columns, content: {
                     ForEach(MainInformation.Category.allCases, id: \.self) { category in
-                        ZStack {
-                            Image(category.rawValue)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                            Text(category.rawValue)
-                                .font(.title)
-                        }
-                        
+                        CategoryView(category: category)
                     }
                 })
                 .navigationTitle("Categories")
             }
+        }
+    }
+}
+
+struct CategoryView: View {
+    let category: MainInformation.Category
+    
+    var body: some View {
+        ZStack {
+            Image(category.rawValue)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .opacity(0.35)
+            Text(category.rawValue)
+                .font(.title)
         }
     }
 }
