8edd51a9b843230de44168164d82b0aa8d96d6bd
wao218 <wesleyosborne1@gmail.com>
2023-10-25 15:53:35 -0700

    Add ModifyIngredientView and ModifyIngredientsView

---
+++ Cookcademy/Views/ModifyRecipes/ModifyIngredientView.swift
@@ -0,0 +1,18 @@
+//
+//  ModifyIngredientView.swift
+//  Cookcademy
+//
+//  Created by wesley osborne on 10/25/23.
+//
+
+import SwiftUI
+
+struct ModifyIngredientView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModifyIngredientView()
+}
---
+++ Cookcademy/Views/ModifyRecipes/ModifyIngredientsView.swift
@@ -0,0 +1,18 @@
+//
+//  ModifyIngredientsView.swift
+//  Cookcademy
+//
+//  Created by wesley osborne on 10/25/23.
+//
+
+import SwiftUI
+
+struct ModifyIngredientsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModifyIngredientsView()
+}
